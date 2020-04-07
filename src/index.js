566eaa4cdeec81ac1d95aae32586a6586d94fa7e
shiminshen <smshen17@gmail.com>
2020-04-08 04:04:31 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -23,55 +23,53 @@ app.post('/webhook', line.middleware(config), async (req, res) => {
       const houseData = await api.getHouse()
       console.log(houseData[0]);
       client.replyMessage(event.replyToken, {
-        {
-          "type": "template",
-          "altText": "this is a carousel template",
-          "template": {
-            "type": "carousel",
-            "columns": [
-              {
-                "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
-                "imageBackgroundColor": "#FFFFFF",
-                "title": "this is menu",
-                "text": "description",
-                "defaultAction": {
-                  "type": "uri",
-                  "label": "View detail",
-                  "uri": "http://example.com/page/123"
-                }
+        "type": "template",
+        "altText": "this is a carousel template",
+        "template": {
+          "type": "carousel",
+          "columns": [
+            {
+              "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+              "imageBackgroundColor": "#FFFFFF",
+              "title": "this is menu",
+              "text": "description",
+              "defaultAction": {
+                "type": "uri",
+                "label": "View detail",
+                "uri": "http://example.com/page/123"
+              }
+            },
+            {
+              "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+              "imageBackgroundColor": "#000000",
+              "title": "this is menu",
+              "text": "description",
+              "defaultAction": {
+                "type": "uri",
+                "label": "View detail",
+                "uri": "http://example.com/page/222"
               },
-              {
-                "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
-                "imageBackgroundColor": "#000000",
-                "title": "this is menu",
-                "text": "description",
-                "defaultAction": {
+              "actions": [
+                {
+                  "type": "postback",
+                  "label": "Buy",
+                  "data": "action=buy&itemid=222"
+                },
+                {
+                  "type": "postback",
+                  "label": "Add to cart",
+                  "data": "action=add&itemid=222"
+                },
+                {
                   "type": "uri",
                   "label": "View detail",
                   "uri": "http://example.com/page/222"
-                },
-                "actions": [
-                  {
-                    "type": "postback",
-                    "label": "Buy",
-                    "data": "action=buy&itemid=222"
-                  },
-                  {
-                    "type": "postback",
-                    "label": "Add to cart",
-                    "data": "action=add&itemid=222"
-                  },
-                  {
-                    "type": "uri",
-                    "label": "View detail",
-                    "uri": "http://example.com/page/222"
-                  }
-                ]
-              }
-            ],
-            "imageAspectRatio": "rectangle",
-            "imageSize": "cover"
-          }
+                }
+              ]
+            }
+          ],
+          "imageAspectRatio": "rectangle",
+          "imageSize": "cover"
         } 
       });
     }
