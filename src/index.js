6bcc314eabef512e810b70bb79fa003e375b4801
shiminshen <smshen17@gmail.com>
2020-04-08 02:12:40 +0800

    add linebot

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-const fetch = require('node-fetch');
+// 引用linebot SDK
+var linebot = require('linebot');
 
-const getHouse = async () => {
+// 用於辨識Line Channel的資訊
+var bot = linebot({
+  channelId: '1654051232',
+  channelSecret: 'c1d2bab6c05c09198e296ffd4e3040e2',
+  channelAccessToken: 'zPZ+AMy9vXfzrJ9AmJxkxdQ+I8EAANlYFFrHzw312BhBoGCVDr0fvEPMt99waz8Qnq1RovA+HrLCn+OOvYA7/OREM2kC0+Z7DAwH0NcGGAP917PIYp1axvXsHPnWqC57+OTRkUXipPR0MN2y6lhACwdB04t89/1O/w1cDnyilFU='
+});
 
-  const res = await fetch('https://rent.591.com.tw/?kind=1&mrtline=100&mrtline=100&order=posttime&orderType=desc&region=1&mrt=1&mrtcoods=4187,4186,4185,4320&rentprice=4&pattern=3')
-  const cookie = res.headers.raw()['set-cookie'].find(h => /^591_new_session/.test(h))
-  const content = await res.text();
-  const token = content.match(/csrf-token" content="(.*)">/)[1]
-  
-  const searchResult = await fetch('https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=1&searchtype=4&mrtline=100&region=1&mrt=1&rentprice=4&patternMore=3,4&order=posttime&orderType=desc&total=50', {
-    headers: {
-      'X-CSRF-TOKEN': token,
-      'Cookie': cookie,
-      'X-Requested-With': 'XMLHttpRequest'
-    }
-  })
-	.then(res => res.json())
-  console.log(searchResult.data.data.length);
-}
+// 當有人傳送訊息給Bot時
+bot.on('message',  (event) => {
+  event.reply(event.message.text).then( (data) => {
+  }).catch( (error) => {
+  });
+});
 
-getHouse()
+const PORT = 3000;
+bot.listen('/linewebhook', PORT, () => {
+    console.log(`bot is active at localhost:${PORT}`);
+});
