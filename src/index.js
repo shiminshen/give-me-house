141c36354ce37bd9dd07fe5e28fec7af741c88e5
shiminshen <smshen17@gmail.com>
2020-04-08 01:25:14 +0800

    create src folder

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const getHouse = async () => {
   const content = await res.text();
   const token = content.match(/csrf-token" content="(.*)">/)[1]
   
-  const searchResult = await fetch('https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=1&searchtype=4&mrtline=100&order=posttime&orderType=desc&region=1&mrt=1&rentprice=4&pattern=3&mrtcoods=4320', {
+  const searchResult = await fetch('https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=1&searchtype=4&mrtline=100&region=1&mrt=1&rentprice=4&patternMore=3,4&order=posttime&orderType=desc&total=50', {
     headers: {
       'X-CSRF-TOKEN': token,
       'Cookie': cookie,
@@ -15,6 +15,7 @@ const getHouse = async () => {
     }
   })
 	.then(res => res.json())
+  console.log(searchResult.data.data.length);
 }
 
 getHouse()
