732e0d993690615f7d65e63a221e01461d4643a3
shiminshen <smshen17@gmail.com>
2020-04-08 17:23:17 +0800

    update query

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
 const fetch = require('node-fetch');
 
-const getHouse = async (searchURL = 'https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=1&searchtype=4&mrtline=100&patternMore=3,4&region=1&mrt=1&rentpriceMore=4,5&patternMore=3,4&not_cover=1&mrtcoods=4318,4319,4320,4321,4282&order=posttime&orderType=desc') => {
+const mrtcoods = '4315,4316,4317,4318,4319,4320,4321,4282,4323,4324,4185,4186,4187'
+const url = `https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=1&searchtype=4&mrtline=100&patternMore=3,4&region=1&mrt=1&rentpriceMore=4,5&patternMore=3,4&not_cover=1&mrtcoods=${mrtcoods}&order=posttime&orderType=desc`
+
+const getHouse = async (searchURL = url) => {
 
   const res = await fetch('https://rent.591.com.tw/?kind=1&mrtline=100&mrtline=100&order=posttime&orderType=desc&region=1&mrt=1&mrtcoods=4187,4186,4185,4320&rentprice=4&pattern=3')
   const cookie = res.headers.raw()['set-cookie'].find(h => /^591_new_session/.test(h))
