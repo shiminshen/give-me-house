6c17107c9d7a29ff5d68f1193b74e2ae3c4114d6
shiminshen <smshen17@gmail.com>
2020-04-08 17:54:53 +0800

    text setInterval

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const line = require('@line/bot-sdk');
 const express = require('express');
 const api = require('./api');
 
+const wholeNewLifeId = 'C2f975b0dd7905f0e8f4da6db716f60df'
+
 // create LINE SDK config from env variables
 const config = {
   channelSecret: 'c1d2bab6c05c09198e296ffd4e3040e2',
@@ -9,11 +11,52 @@ const config = {
 };
 const client = new line.Client(config);
 
-client.pushMessage('C2f975b0dd7905f0e8f4da6db716f60df', { type: 'text', text: '吃'  })
+const generateMessage = (data) => {
+  const columns = data
+  .filter(h => h.distance < 2500)
+  .slice(0,10)
+  .map(h => ({
+    "thumbnailImageUrl": h.cover,
+    "imageBackgroundColor": "#FFFFFF",
+    "title": h.address_img,
+    "text": `$${h.price}\n${h.layout}\n${h.posttime}\n${h.search_name} ${h.distance}m`,
+    "actions": [
+      {
+        "type": "uri",
+        "label": "View detail",
+        "uri": `https://rent.591.com.tw/rent-detail-${h.id}.html`
+      }
+    ]
+  }))
+
+  return {
+    type: "template",
+    altText: "this is a carousel template",
+    template: {
+      type: "carousel",
+      columns,
+      imageAspectRatio: "rectangle",
+      imageSize: "cover"
+    }
+  }
+};
+
+setInterval(() => {
+  const currTime = new Date().getTime() / 1000
+  const houseData = await api.getHouse()
+  // only get data in last one hour
+  const newData = houseData.filter(h => ((currTime - h.updatetime) / 3600) < 1)
+  if (newData.length) {
+    const message = generateMessage(newData)
+    client.pushMessage(wholeNewLifeId, { type: 'text', text: '有新房子囉!!!' })
+    client.pushMessage(wholeNewLifeId, message)
+  }
+
+}, 3600000)
+
 const app = express();
 app.post('/webhook', line.middleware(config), async (req, res) => {
   const event = req.body.events[0];
-  console.log(event.source);
 
   if (event.type === 'message') {
     const message = event.message;
@@ -22,33 +65,8 @@ app.post('/webhook', line.middleware(config), async (req, res) => {
 
     if (message.type === 'text' && message.text === 'fuck') {
       const houseData = await api.getHouse()
-      console.log(houseData);
-      const messages = houseData
-        .filter(h => h.distance < 2500)
-        .slice(0,10)
-        .map(h => ({
-        "thumbnailImageUrl": h.cover,
-        "imageBackgroundColor": "#FFFFFF",
-        "title": h.address_img,
-        "text": `$${h.price}\n${h.layout}\n${h.posttime}\n${h.search_name} ${h.distance}m`,
-        "actions": [
-          {
-            "type": "uri",
-            "label": "View detail",
-            "uri": `https://rent.591.com.tw/rent-detail-${h.id}.html`
-          }
-        ]
-      }))
-      client.replyMessage(event.replyToken, {
-        "type": "template",
-        "altText": "this is a carousel template",
-        "template": {
-          "type": "carousel",
-          "columns": messages,
-          "imageAspectRatio": "rectangle",
-          "imageSize": "cover"
-        }
-      });
+      const message = generateMessage(houseData)
+      client.replyMessage(event.replyToken, message);
     }
   }
   res.json({})
