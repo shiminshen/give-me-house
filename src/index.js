49182c87c68f399a607097e2e19ccb59c04cda39
shiminshen <smshen17@gmail.com>
2020-04-09 02:16:13 +0800

    add prettier and eslint

--- src/index.js
+++ src/index.js
@@ -1,33 +1,34 @@
-const line = require('@line/bot-sdk');
-const express = require('express');
-const api = require('./api');
+const line = require("@line/bot-sdk");
+const express = require("express");
+const api = require("./api");
 
-const wholeNewLifeId = 'C2f975b0dd7905f0e8f4da6db716f60df'
+const wholeNewLifeId = "C2f975b0dd7905f0e8f4da6db716f60df";
 
 // create LINE SDK config from env variables
 const config = {
-  channelSecret: 'c1d2bab6c05c09198e296ffd4e3040e2',
-  channelAccessToken: 'zPZ+AMy9vXfzrJ9AmJxkxdQ+I8EAANlYFFrHzw312BhBoGCVDr0fvEPMt99waz8Qnq1RovA+HrLCn+OOvYA7/OREM2kC0+Z7DAwH0NcGGAP917PIYp1axvXsHPnWqC57+OTRkUXipPR0MN2y6lhACwdB04t89/1O/w1cDnyilFU='
+  channelSecret: "c1d2bab6c05c09198e296ffd4e3040e2",
+  channelAccessToken:
+    "zPZ+AMy9vXfzrJ9AmJxkxdQ+I8EAANlYFFrHzw312BhBoGCVDr0fvEPMt99waz8Qnq1RovA+HrLCn+OOvYA7/OREM2kC0+Z7DAwH0NcGGAP917PIYp1axvXsHPnWqC57+OTRkUXipPR0MN2y6lhACwdB04t89/1O/w1cDnyilFU=",
 };
 const client = new line.Client(config);
 
 const generateMessage = (data) => {
   const columns = data
-  .filter(h => h.distance < 2500)
-  .slice(0,10)
-  .map(h => ({
-    "thumbnailImageUrl": h.cover,
-    "imageBackgroundColor": "#FFFFFF",
-    "title": h.address_img,
-    "text": `$${h.price}\n${h.layout}\n${h.posttime}\n${h.search_name} ${h.distance}m`,
-    "actions": [
-      {
-        "type": "uri",
-        "label": "View detail",
-        "uri": `https://rent.591.com.tw/rent-detail-${h.id}.html`
-      }
-    ]
-  }))
+    .filter((h) => h.distance < 2500)
+    .slice(0, 10)
+    .map((h) => ({
+      thumbnailImageUrl: h.cover,
+      imageBackgroundColor: "#FFFFFF",
+      title: h.address_img,
+      text: `$${h.price}\n${h.layout}\n${h.posttime}\n${h.search_name} ${h.distance}m`,
+      actions: [
+        {
+          type: "uri",
+          label: "View detail",
+          uri: `https://rent.591.com.tw/rent-detail-${h.id}.html`,
+        },
+      ],
+    }));
 
   return {
     type: "template",
@@ -36,39 +37,41 @@ const generateMessage = (data) => {
       type: "carousel",
       columns,
       imageAspectRatio: "rectangle",
-      imageSize: "cover"
-    }
-  }
+      imageSize: "cover",
+    },
+  };
 };
 
 setInterval(async () => {
-  const currTime = new Date().getTime() / 1000
-  const houseData = await api.getHouse()
+  const currTime = new Date().getTime() / 1000;
+  const houseData = await api.getHouse();
   // only get data in last one hour
-  const newData = houseData.filter(h => ((currTime - h.updatetime) / 3600) < 1)
+  const newData = houseData.filter((h) => (currTime - h.updatetime) / 3600 < 1);
   if (newData.length) {
-    const message = generateMessage(newData)
-    client.pushMessage(wholeNewLifeId, [{ type: 'text', text: '有新房子囉!!!' }, message ])
+    const message = generateMessage(newData);
+    client.pushMessage(wholeNewLifeId, [
+      { type: "text", text: "有新房子囉!!!" },
+      message,
+    ]);
   }
-
-}, 3600000)
+}, 3600000);
 
 const app = express();
-app.post('/webhook', line.middleware(config), async (req, res) => {
+app.post("/webhook", line.middleware(config), async (req, res) => {
   const event = req.body.events[0];
 
-  if (event.type === 'message') {
+  if (event.type === "message") {
     const message = event.message;
-    console.log('message');
+    console.log("message");
     console.log(message);
 
-    if (message.type === 'text' && message.text === 'fuck') {
-      const houseData = await api.getHouse()
-      const message = generateMessage(houseData)
+    if (message.type === "text" && message.text === "fuck") {
+      const houseData = await api.getHouse();
+      const message = generateMessage(houseData);
       client.replyMessage(event.replyToken, message);
     }
   }
-  res.json({})
+  res.json({});
 });
 
 // listen on port
