48361d53b41d03aff3cd8905a3684b9b4e226ef6
shiminshen <smshen17@gmail.com>
2020-04-09 23:23:40 +0800

    add 內湖

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 
 // const mrtcoods = "4315,4316,4317,4318,4319,4320,4321,4282,4323,4324,4185,4186,4187";
-const mrtcoods = "4320,4321,4282,4323,4319";
+const mrtcoods = "4320,4321,4282,4323,4319, 4318";
 const url = `https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=1&searchtype=4&mrtline=100&patternMore=3,4&order=posttime&orderType=desc&region=1&mrt=1&mrtcoods=${mrtcoods}&rentpriceMore=4,5&patternMore=3,4`;
 
 const getHouse = async (searchURL = url) => {
